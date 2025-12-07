8fb2720f0d8be16c9e24d7aa1ec47e937d39e415
Jhon Hernández <jhobahego16@hotmail.com>
2025-12-07 13:52:01 -0500

    refactor(consts): format UI_MESSAGES and GAME_CONFIG for consistency

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,21 +1,30 @@
 export const UI_MESSAGES = {
-    LOADING: {
-        STORY: "Generando historia...",
-        IMAGE: "Generando imagen...",
-    },
-    ERRORS: {
-        STORY: "Error al generar la historia. Por favor, intenta de nuevo.",
-        IMAGE: "Error al generar la imagen. Por favor, intenta de nuevo.",
-        MISING_PROMPT: "Falta el prompt para generar la historia.",
-    },
-    PLACEHOLDERS: {
-        INPUT: "Describe que quieres hacer, adónde ir, que examinar o cómo reaccionar...",
-    }
-}
+  LOADING: {
+    STORY: "Generando historia...",
+    IMAGE: "Generando imagen...",
+  },
+  ERRORS: {
+    STORY: "Error al generar la historia. Por favor, intenta de nuevo.",
+    IMAGE: "Error al generar la imagen. Por favor, intenta de nuevo.",
+    MISING_PROMPT: "Falta el prompt para generar la historia.",
+  },
+  PLACEHOLDERS: {
+    INPUT:
+      "Describe que quieres hacer, adónde ir, que examinar o cómo reaccionar...",
+  },
+};
 
 export const GAME_CONFIG = {
-    IMAGE: {
-        DEFAULT_PROMPT: "A pixel art style image in 16:9 aspect ratio of a post-apocalyptic world with zombies and limited resources, evoking a sense of survival and adventure.",
-        SEPARATOR: "IMAGEN:",
-    }
-}
+  IMAGE: {
+    DEFAULT_PROMPT:
+      "A pixel art style image in 16:9 aspect ratio of a post-apocalyptic world with zombies and limited resources, evoking a sense of survival and adventure.",
+    SEPARATOR: "IMAGEN:",
+  },
+  AUDIO: {
+    AUTO_PLAY: true,
+    // Voces disponibles
+    // Male Voices: "es-ES-Chirp3-HD-Algenib", "es-ES-Chirp-HD-D"
+    // Female Voices: "es-ES-Chirp-HD-F"
+    VOICE: "es-ES-Chirp-HD-F",
+  },
+};
